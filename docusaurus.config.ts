365d9d7f2288cca2be627743ab2e5f379b85276e
Manuel <30572287+manuel-rw@users.noreply.github.com>
2023-12-22 20:07:49 +0100

    fix: image zoom selector selecting link elements

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from '@docusaurus/types';
+import type { Config, PluginOptions } from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 import { themes as prismThemes } from 'prism-react-renderer';
 import { EnumChangefreq } from 'sitemap';
@@ -204,7 +204,7 @@ const config: Config = {
       },
     ],
     imageZoom: {
-      selector: '.markdown :not(em) > img',
+      selector: '.markdown :not(em):not(a) > img'
     },
     tableOfContents: {
       minHeadingLevel: 2,
