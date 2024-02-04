46760c965fddac644ba45d3d8ec0261e472122f9
Manuel <30572287+manuel-rw@users.noreply.github.com>
2024-02-04 09:42:06 +0100

    config: add stylesheet for argos

--- tests/screenshot.spec.ts
+++ tests/screenshot.spec.ts
@@ -5,7 +5,7 @@ import {extractSitemapPathnames, pathnameToArgosName} from './utils';
 
 // Constants
 const siteUrl = 'http://localhost:3000';
-const sitemapPath = './build/sitemap.xml';
+const sitemapPath = '../build/sitemap.xml';
 const stylesheetPath = './screenshot.css';
 const stylesheet = fs.readFileSync(stylesheetPath).toString();
 
