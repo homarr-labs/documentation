411aacddbc322e93b7607e4c4a6a50ba2f35a33e
Manuel <manuel.ruwe@protonmail.com>
2024-08-04 16:33:06 +0200

    fix: upload images

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,12 +6,6 @@ const config: PlaywrightTestConfig = {
     port: 3000,
     command: 'yarn docusaurus serve',
   },
-  reporter: [
-    // Use "dot" reporter on CI, "list" otherwise (Playwright default).
-    process.env.CI ? ["dot"] : ["list"],
-    // Add Argos reporter. Upload on CI only.
-    ["@argos-ci/playwright/reporter", { uploadToArgos: !!process.env.CI }],
-  ],
   use: {
     trace: 'on-first-retry',
     screenshot: "only-on-failure",
