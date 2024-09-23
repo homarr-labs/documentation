b35dd5fea923546e1ee924eea23efb0bbc20de20
Manuel <30572287+manuel-rw@users.noreply.github.com>
2024-09-23 17:40:15 +0200

    config: add authors (#112)

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -59,6 +59,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl: 'https://github.com/ajnart/homarr-docs/edit/master',
+          authorsMapPath: "authors.yml"
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
