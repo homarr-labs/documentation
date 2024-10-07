a87db8d774e1b0935c9791a7bd19f15e2284e38c
Manuel <30572287+manuel-rw@users.noreply.github.com>
2024-10-07 22:33:40 +0200

    feat: improve design of dataflow

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '128': '32rem'
+      }
+    },
   },
   darkMode: ['class', '[data-theme="dark"]'],
   plugins: [],
