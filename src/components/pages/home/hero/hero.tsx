6630f5deb50f27c9cf00646c42388b4188c22c24
Manuel <30572287+manuel-rw@users.noreply.github.com>
2025-03-25 19:12:20 +0100

    feat: replace container with flexbox

--- src/components/pages/home/hero/hero.tsx
+++ src/components/pages/home/hero/hero.tsx
@@ -8,30 +8,29 @@ export default function HomeHero() {
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner, styles.hero)}>
       {/* TODO: remove container and add more columns to the grid */}
-      <div className="container" style={{ zIndex: 1 }}>
-        <div className="row">
-          <div className="col">
-            <h1 className="font-extrabold lg:text-7xl text-3xl">A simple, yet<br />powerful dashboard<br />for your server.</h1>
-            <p className={'lg:text-2xl text-base'}>
-              A sleek, modern dashboard that puts all of your apps and services at your fingertips.
-              Control everything in one convenient location. Seamlessly integrates with the
-              apps you've added, providing you with valuable information.
-            </p>
+      <div className="flex">
+        <div className="flex-grow-0 ps-0 lg:ps-52">
+          <h1 className="font-extrabold lg:text-7xl text-3xl">A simple, yet<br />powerful dashboard<br />for your
+            server.</h1>
+          <p className={'lg:text-2xl text-base'}>
+            A sleek, modern dashboard that puts all of your apps and services at your fingertips.
+            Control everything in one convenient location. Seamlessly integrates with the
+            apps you've added, providing you with valuable information.
+          </p>
 
-            <Link
-              data-umami-event="Install button"
-              className={'button button--secondary button--lg rounded-3xl dark:border-zinc-600 dark:bg-zinc-800'}
-              to="/docs/getting-started"
-            >
-              <div className={'flex items-center gap-3'}>
-                <span className={'dark:text-gray-200'}>Install</span>
-                <IconDownload className={'dark:text-gray-200'} size={20} />
-              </div>
-            </Link>
-          </div>
-          <div className="col lg:block hidden hover-animation">
-            <HeroCards />
-          </div>
+          <Link
+            data-umami-event="Install button"
+            className={'button button--secondary button--lg rounded-3xl dark:border-zinc-600 dark:bg-zinc-800'}
+            to="/docs/getting-started"
+          >
+            <div className={'flex items-center gap-3'}>
+              <span className={'dark:text-gray-200'}>Install</span>
+              <IconDownload className={'dark:text-gray-200'} size={20} />
+            </div>
+          </Link>
+        </div>
+        <div className="flex-grow-1 lg:block hidden hover-animation">
+          <HeroCards />
         </div>
       </div>
     </header>
