ec4e2ba63fd787c381ff59d2c8baeaa7b3d52eb1
Manuel <30572287+manuel-rw@users.noreply.github.com>
2025-03-25 19:44:41 +0100

    feat: add stock trend card

--- src/components/pages/home/hero/hero-cards.tsx
+++ src/components/pages/home/hero/hero-cards.tsx
@@ -1,43 +1,11 @@
 import { IconArrowDownRight, IconArrowUpRight, IconCloud, IconWind } from '@tabler/icons-react';
-import { ReactNode, useState, useEffect } from 'react';
+import { ReactNode } from 'react';
 import clsx from 'clsx';
-
-interface Download {
-  filename: string;
-  progress: number;
-}
-
-const generateRandomDownload = (): Download => {
-  return {
-    filename: `${Date.now()}-${Math.random().toString(36).slice(-2)}.mkv`,
-    progress: 0,
-  };
-};
+import { DownloadsCard } from '@site/src/components/pages/home/hero/cards/downloads';
+import { StockTrendCard } from '@site/src/components/pages/home/hero/cards/stock-trend';
 
 export const HeroCards = () => {
-  const [downloads, setDownloads] = useState<Download[]>([]);
 
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setDownloads((prev: Download[]) => {
-        const filteredPrev = prev.filter(download => download.progress !== 100);
-
-        filteredPrev.forEach((download, index) => {
-          download.progress = Math.min(100, download.progress + getRndInteger(0, index < 2 ? 16 : 8));
-        });
-
-        if (filteredPrev.length < 3 && filteredPrev.length === prev.length) {
-          filteredPrev.push(generateRandomDownload());
-        }
-
-        return [...filteredPrev];
-      });
-    }, 500);
-
-    return () => {
-      clearInterval(intervalId);
-    };
-  }, []);
 
   return (
     <div className="grid grid-cols-3 grid-rows-2 gap-4 h-full hero-cards">
@@ -58,36 +26,16 @@ export const HeroCards = () => {
         </div>
       </Card>
       <Card additionalClassNames={'col-span-2 text-xs'} centered={false} padding={false}>
-        <table className="mb-0">
-          <thead>
-          <tr>
-            <th className="border-none">File</th>
-            <th className="border-none w-full">Progress</th>
-          </tr>
-          </thead>
-          <tbody>
-          {downloads.map((file: Download, index: number) => {
-            return (
-              <tr key={index}>
-                <td className="border-none text-nowrap">{file.filename}</td>
-                <td className="border-none w-full">
-                  <div className="overflow-hidden rounded-xl h-3 w-full bg-zinc-600">
-                    <div className="bg-green-600 h-full animated-width" style={{ width: `${file.progress}%` }}></div>
-                  </div>
-                </td>
-              </tr>
-            );
-          })}
-          </tbody>
-        </table>
+        <DownloadsCard />
       </Card>
-      <Card additionalClassNames={'col-span-2'} padding={false}>
-        <video src={"https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"} muted={true} autoPlay={true} controls={false}></video>
+      <Card additionalClassNames={'col-span-2 relative'} padding={false}>
+        <StockTrendCard />
       </Card>
       <Card>
-        <div className={"w-full flex flex-col items-center gap-4 app-card"}>
-          <img src={"https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/svg/prowlarr.svg"} className="aspect-square w-32 h-32" alt={"Prowlarr Icon"} />
-          <span className={"text-lg font-bold"}>Prowlarr</span>
+        <div className={'w-full flex flex-col items-center gap-4 app-card'}>
+          <img src={'https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/svg/prowlarr.svg'}
+               className="aspect-square w-32 h-32" alt={'Prowlarr Icon'} />
+          <span className={'text-lg font-bold'}>Prowlarr</span>
         </div>
       </Card>
     </div>
@@ -108,7 +56,3 @@ const Card = ({ children, additionalClassNames, centered = true, padding = true 
     </div>
   );
 };
-
-function getRndInteger(min: number, max: number) {
-  return Math.floor(Math.random() * (max - min)) + min;
-}
