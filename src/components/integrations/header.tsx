90f7f5454de0b42b4efa1b0ab6651727a34041db
Meier Lukas <meierschlumpf@gmail.com>
2025-07-12 11:53:55 +0200

    feat(widgets): add releases widget

--- src/components/integrations/header.tsx
+++ src/components/integrations/header.tsx
@@ -1,5 +1,6 @@
 import { IntegrationDefinition } from '@site/src/types';
 import { DocsHeader } from '../ui/header';
+import { useColorMode } from '@docusaurus/theme-common';
 
 interface IntegrationHeaderProps {
   integration: IntegrationDefinition;
@@ -7,12 +8,26 @@ interface IntegrationHeaderProps {
 }
 
 export const IntegrationHeader = (props: IntegrationHeaderProps) => {
+  const { isDarkTheme } = useColorMode();
+
   return (
     <DocsHeader
       title={props.integration.name}
       description={props.integration.description}
-      icon={<img src={props.integration.iconUrl} width={48} height={48} />}
+      icon={
+        <img src={getIntegrationIconUrl(props.integration, isDarkTheme)} width={48} height={48} />
+      }
       categories={props.categories}
     />
   );
 };
+
+export const getIntegrationIconUrl = (
+  integration: IntegrationDefinition,
+  isDarkTheme: boolean
+): string =>
+  typeof integration.iconUrl === 'string'
+    ? integration.iconUrl
+    : isDarkTheme
+      ? integration.iconUrl.dark
+      : integration.iconUrl.light;
--- src/components/widgets/configuration.tsx
+++ src/components/widgets/configuration.tsx
@@ -5,9 +5,19 @@ interface WidgetConfigProps {
 }
 
 export const WidgetConfig = (props: WidgetConfigProps) => {
+  const innerConfigs = getInnerConfigs(props.configuration);
+
   return (
     <div>
       <ConfigTable configuration={props.configuration} />
+
+      {innerConfigs.map((item) => (
+        <>
+          <h3 id={item.name}>{item.name}</h3>
+
+          <ConfigTable configuration={item.configuration} />
+        </>
+      ))}
     </div>
   );
 };
@@ -56,4 +66,23 @@ const ConfigValues = ({ values }: Pick<WidgetConfigurationItem, 'values'>) => {
   const reference = <a href={`#${values.name}`}>{values.name}</a>;
   if (values.type === 'object') return reference;
   if (values.type === 'array') return <span>{reference}[]</span>;
+};
+
+const getInnerConfigs = (config: WidgetConfiguration) => {
+  const configItems: Extract<WidgetConfigurationItem['values'], { type: 'object' | 'array' }>[] =
+    [];
+  config.items.forEach((item) => {
+    if (
+      item.values &&
+      typeof item.values === 'object' &&
+      (item.values.type === 'object' || item.values.type === 'array')
+    ) {
+      configItems.push(item.values);
+      const innerItems = getInnerConfigs(item.values.configuration);
+      if (innerItems.length > 0) {
+        configItems.push(...innerItems);
+      }
+    }
+  });
+  return configItems;
 };
