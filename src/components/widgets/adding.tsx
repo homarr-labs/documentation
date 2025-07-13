44551ba430adb377a9d271bdeea45449ba800f07
Meier Lukas <meierschlumpf@gmail.com>
2025-07-13 13:25:18 +0200

    fix(widgets): change adding link to already existing getting-started

--- src/components/widgets/adding.tsx
+++ src/components/widgets/adding.tsx
@@ -2,7 +2,10 @@ export const AddingWidget = () => {
   return (
     <p>
       You can find how to add the widget on the{' '}
-      <a href="/docs/board/widgets#adding-widgets">Widgets</a> documentation page.
+      <a href="../../getting-started/after-the-installation#embed-integration-data-using-widgets">
+        Widgets
+      </a>{' '}
+      documentation page.
     </p>
   );
 };
