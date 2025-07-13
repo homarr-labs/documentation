40456db1be54d4cd647e5ed03b0166212c8aa8aa
Meier Lukas <meierschlumpf@gmail.com>
2025-07-13 13:17:06 +0200

    fix(widgets): move widgets2 to widgets directory

--- docs/widgets2/media-releases/index.ts
+++
@@ -1,9 +0,0 @@
-import { WidgetDefinition } from '@site/src/types';
-import { IconTicket } from '@tabler/icons-react';
-
-export const mediaReleasesWidget: WidgetDefinition = {
-  icon: IconTicket,
-  name: 'Media releases',
-  description: 'Display newly added medias or upcoming releases from different integrations',
-  path: '../../widgets2/media-releases',
-};
