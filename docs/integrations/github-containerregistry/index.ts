f71459986ee8a3d40810b7932560e3d57b3eed92
Andre Silva <32734153+Aandree5@users.noreply.github.com>
2025-08-01 12:01:34 +0100

    refactor: renamed `GithubPackages` to `GitHubContainerRegistry` (#345)
    Co-authored-by: Meier Lukas <meierschlumpf@gmail.com>

--- docs/integrations/github-containerregistry/index.ts
+++ docs/integrations/github-containerregistry/index.ts
@@ -1,8 +1,8 @@
 import { IntegrationDefinition } from '@site/src/types';
 
-export const githubPackagesIntegration: IntegrationDefinition = {
-  name: 'GitHub Packages',
-  description: 'GitHub Packages is a platform for hosting and managing packages.',
+export const gitHubContainerRegistryIntegration: IntegrationDefinition = {
+  name: 'GitHub Container Registry',
+  description: 'GitHub Container Registry is a platform for hosting and managing packages.',
   iconUrl: {
     light: 'https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/svg/github.svg',
     dark: 'https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/svg/github-light.svg',
