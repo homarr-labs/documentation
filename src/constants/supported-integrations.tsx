2588b04e2ef0a717ea8aedd46a58a8a5ce6cf735
Daniel Lopes <daniel.lopes@agido.com>
2026-05-30 13:08:17 +0200

    feat: add Technitium DNS integration, usable with DNS Hole Controls and DNS Hole Summary widgets

--- src/constants/supported-integrations.tsx
+++ src/constants/supported-integrations.tsx
@@ -20,6 +20,7 @@ export const supportedIntegrations = [
   { iconUrl: 'https://cdn.jsdelivr.net/gh/loganmarchione/homelab-svg-assets/assets/docker.svg', name: 'docker' },
   { iconUrl: 'https://cdn.jsdelivr.net/gh/walkxcode/dashboard-icons/svg/jellyseerr.svg', name: 'Jellyseerr' },
   { iconUrl: 'https://cdn.jsdelivr.net/gh/loganmarchione/homelab-svg-assets/assets/adguardhome.svg', name: 'AdguardHome' },
+  { iconUrl: 'https://cdn.jsdelivr.net/gh/selfhst/icons/svg/technitium.svg', name: 'Technitium' },
   { iconUrl: 'https://cdn.jsdelivr.net/gh/walkxcode/dashboard-icons/png/tdarr.png', name: 'Tdarr' },
   { iconUrl: 'https://cdn.jsdelivr.net/gh/walkxcode/dashboard-icons/svg/prowlarr.svg', name: 'Prowlarr' },
   { iconUrl: 'https://cdn.jsdelivr.net/gh/walkxcode/dashboard-icons/svg/ical.svg', name: 'iCal' },
